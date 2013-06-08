0f71a86275edd620da2fd0747c860328fc88d77f
Petra Petrich <petra.petrich@gmail.com>
2013-06-08 14:15:14 +0200

    promjenili color space u HSV, dodali switcheve za izlaz i pauzu. Te napravili mouse callback koji definira ROI

--- Program.cpp
+++ Program.cpp
@@ -3,34 +3,90 @@
 
 #include <stdio.h>
 
-using namespace cv;
+//using namespace cv;
 using namespace std;
 
 static void help()
 {
-    printf("\nPerica perich i makija makich \n"
-           "    /.Program [video name]\n\n");
+    cout << "\nPerica perich i makija makich \n"
+           "    /.Program [video name]\n\n";
 }
+cv::Rect box;
+bool drawing_box = false;
+
+void defineRoi( cv::Mat& img, cv::Rect rect ){
+    cout << "crtam" << endl;
+    cv::rectangle( img, rect.tl(), rect.br(), cv::Scalar(255,0,0), 1);
+    imshow("Video", img);
+
+} 
+
+void onMouse( int event, int x, int y, int flags, void* param ) {
+    cv::Mat& image = *(cv::Mat*) param;
+    switch( event )
+    {
+        case CV_EVENT_LBUTTONDOWN:
+            drawing_box = true;
+            box = cv::Rect(x, y, 0, 0);
+            break;
+        case CV_EVENT_MOUSEMOVE: 
+            if( drawing_box ) {
+                box.width = x-box.x;
+                box.height = y-box.y;
+            }
+            break;
+        case CV_EVENT_LBUTTONUP: 
+            drawing_box = false;
+            if( box.width<0 ) {
+                box.x+=box.width;
+                box.width *= -1;
+            }
+            if( box.height<0 ) {
+                box.y+=box.height;
+                box.height*=-1;
+            }
+            defineRoi(image, box );
+            cout << "box coordinates \n" 
+                << "x\t y\t height\t width\n"
+                << box.x << "\t" << box.y << "\t" 
+                << box.height << "\t" << box.width << "\n";
+            break;
+    }
+}
+
 
 int main( int argc, const char** argv )
 {
     help();
     
-    VideoCapture cap;
+    cv::VideoCapture cap;
     if(argc > 1) cap.open(string(argv[1]));
     else {
         cout << "Ucitavam zadani video" << endl;
         cap.open("/media/petrich-wd/Faks/Zavrsni rad/Snimke/MVI_0810.MOV");      
     }
     
-    Mat frame;
-    namedWindow("Perin prozor", 1);
+    cv::Mat frame, frame_hsv;
+    cv::namedWindow("Video", 1);
     
     for(;;){
         cap >> frame;
-        if(!frame.data) break;
-        imshow("video", frame);
-        if(waitKey(30) >= 0) break;
+        cv::cvtColor(frame,frame_hsv,CV_BGR2HSV);
+        if(!frame_hsv.data) break;
+        cv::imshow("Video", frame_hsv);
+                
+        int c = cv::waitKey(15);
+        switch( (char)c )
+        {
+            case 27:
+                cout << "Izlazim ... \n ";
+                return 0;
+        
+            case 'p':
+                cout << "Stisni s za start" << endl;
+                cv::setMouseCallback( "Video", onMouse, (void*)&frame_hsv );
+                if (cv::waitKey(0)=='s') break;
+        }
     }
     
     return 0;
